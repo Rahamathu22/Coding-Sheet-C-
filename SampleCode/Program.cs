6319762c6f445a1681a65f17038507e85f8b338c
Rahamath22 <rahamath22104@gmail.com>
2024-01-12 22:14:59 +0530

    GeekForGeek Coding Sheet

---
+++ SampleCode/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Odd or Even
+            //int N = int.Parse(Console.ReadLine());
+            //if (N % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+
+
+            ////Last Digit
+            //int Num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(Num%10);
+
+
+            ////Count the digit
+            //int num = int.Parse(Console.ReadLine());
+            //int Count = 0;
+            //bool value = true;
+            //while (value)
+            //{
+            //    if (num > 0)
+            //    {
+            //        int temp = num % 10;
+            //        Count++;
+            //        num = num / 10;
+            //    }
+            //    else
+            //    {
+            //        value = false;
+            //    }
+
+            //}
+            //Console.WriteLine("Count :" + Count);
+
+            // Reverse the number
+            int Number = int.Parse(Console.ReadLine());
+    
+            while (Number!=0)
+            {
+                if (Number > 0)
+                {
+                    int temp = Number % 10;
+                    Console.Write(temp);
+                    Number = Number / 10;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
